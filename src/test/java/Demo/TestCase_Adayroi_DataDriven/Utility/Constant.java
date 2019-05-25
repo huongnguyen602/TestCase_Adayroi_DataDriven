1a91b5dc21dfe8b3b5a628b494893dfae0174be0
HuongNguyen <kjjery.komo@gmail.com>
2019-05-25 23:56:16 +0700

    + Modify for Path_Driver

--- src/test/java/Demo/TestCase_Adayroi_DataDriven/Utility/Constant.java
+++ src/test/java/Demo/TestCase_Adayroi_DataDriven/Utility/Constant.java
@@ -1,7 +1,7 @@
 package Demo.TestCase_Adayroi_DataDriven.Utility;
 
 public class Constant {
-	public static final String Path_Driver = "..\\drivers\\chromedriver.exe";
+	public static final String Path_Driver = "drivers\\chromedriver.exe";
 	public static final String URL = "https://www.adayroi.com/";
 	public static final String Path_TestData = "..\\TestCase_Adayroi_DataDriven\\TestData\\";
 	public static final String File_TestData = "TestData.xlsx";
